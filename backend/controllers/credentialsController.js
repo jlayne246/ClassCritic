38c21ca061f1ff65ae92be4a22f5ed0f7010306b
K11 <72450222+246-K-11@users.noreply.github.com>
2024-08-12 09:44:59 -0400

    Update credentialsController.js
    added comments to clarify purpose of code segments

--- backend/controllers/credentialsController.js
+++ backend/controllers/credentialsController.js
@@ -1,12 +1,19 @@
 const Credentials = require("../models/credentialModel");
 const mongoose = require("mongoose");
 
+//Function to create a new user at register page
 const createUser = async(req, res) => {
+
+//Decontructs Json from Json object obtained from API post request
+//Sets the following variables below using JSON object with properties of the same nams
     const {
         email,
         password,
     } = req.body;
 
+//Await promise, creating a new database record using the Credentials model
+//email and password properties in the record are set with the request object properties above.
+//If error returned, respond with json object containing the error
     try{
         const register = await Credentials.create({
             email,
@@ -19,6 +26,7 @@ const createUser = async(req, res) => {
     }
 }
 
+//export functions to be used in routing
 module.exports ={
     createUser,
 }
