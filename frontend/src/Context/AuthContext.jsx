fc213e9e0ed74c0297a4f99e5d7105d819afc18c
Joshua Layne <joshualayne246@gmail.com>
2024-08-14 19:42:10 -0400

    Edited to allow for login state to persist across refreshes

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
+const getInitialState = () => {
+    const isLoggedIn = sessionStorage.getItem("currentLogin");
+    return isLoggedIn ? JSON.parse(isLoggedIn) : null
+  }
+
 export const AuthProvider = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(getInitialState());
+
+    useEffect(() => {
+        sessionStorage.setItem("currentLogin", JSON.stringify(isLoggedIn))
+    }, [isLoggedIn])
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
